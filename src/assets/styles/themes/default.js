27ece2172c6abe2ebcb2eefbfd5a52b2e2338cc6
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-31 14:56:27 -0300

    styles: default theme

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      main: '#5061FC',
+      light: '#6674F4',
+      lighter: '#E0E3FF',
+      dark: '#3346F0',
+    },
+
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+      300: '#A1A1A1',
+    },
+
+    danger: {
+      light: '#F97171',
+      main: '#FC5050',
+      dark: '#F63131',
+    },
+  },
+};
