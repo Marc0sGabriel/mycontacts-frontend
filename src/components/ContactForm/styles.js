7e47ebd471d71c88d23070e041f410b07a3980c5
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-31 15:15:21 -0300

    styles: contact form component

---
+++ src/components/ContactForm/styles.js
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const ButtonContainer = styled.div`
+  button {
+    width: 100%;
+  }
+`;
