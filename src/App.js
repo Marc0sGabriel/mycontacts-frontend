4a9c046f3af684bc27651f2db8af7c9171419961
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-31 14:53:41 -0300

    feat: Added theme, components, and routes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import defaultTheme from './assets/styles/themes/default';
+
+import GlobalStyles from './assets/styles/globalStyles';
+import { Container } from './styles';
+import Header from './components/Header';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+        <GlobalStyles />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
