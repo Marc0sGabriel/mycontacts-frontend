f6ecb9df12ad3116f76e336181a01d965dd2313c
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-31 14:49:26 -0300

    feat: utils regex to check phone and email

---
+++ src/utils/isValidEmail.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable operator-linebreak */
+export default function isValidEmail(email) {
+  const regex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  return regex.test(email);
+}
