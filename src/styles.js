d73d950738486d208bfaa2ff040107199c3917fe
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-31 14:55:29 -0300

    styles: added css to App file

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+
+  @media (max-width: 500px) {
+    padding: 0 1.25rem;
+
+    *::placeholder {
+      font-size: 1rem;
+    }
+  }
+`;
